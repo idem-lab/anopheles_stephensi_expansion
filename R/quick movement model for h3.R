e3be9b326361dc2eb5ae5dadd03685a42ddad49e
Nick Golding <goldingn35@gmail.com>
2023-09-04 16:15:20 +1000

    use preprepared hexes in quick mobility model

--- R/quick movement model for h3.R
+++ R/quick movement model for h3.R
@@ -3,56 +3,50 @@
 library(tidyverse)
 library(readr); library(sf); library(terra); library(maptools); library(movement); library(geodata); library(h3);library(reshape2)
 
+hexes <- readRDS("output/hexes.RDS")
 
 #get wopr for all countries
 
 global.pop <- rast("output/rasters/covariates/pop.grd")[["pop_2020"]]
-# global.pop <- rast("data/WorldPop_UNAdj_v3_DRC_fix.2020.Annual.Data.1km.Data.tif")
 
 global.pop <- terra::aggregate(global.pop,
-                               fact = 100,
+                               fact = 10,
                                fun="sum",
                                na.rm = TRUE)
 
 global.pop <- terra::project(global.pop,"ESRI:102022")
-plot(global.pop)
-#get the polygons we want
-library(h3)
-source("R/bounding_box_to_h3.R")
 
 #make a spat vect version for calculating sum of pop
-region_hex_spat_vec <- vect(region_hex)
+region_hex_spat_vec <- vect(hexes)
 
 
 region_hex_pop <- terra::zonal(
   global.pop,
   region_hex_spat_vec,
   fun = sum,
-  na.rm=TRUE)
+  na.rm = TRUE)
 
 #subsets to those with finite population count
 
-populated_hexes <- region_hex[!is.nan(region_hex_pop[,1]),]
-plot(populated_hexes)
-region_hex_pop <- region_hex_pop[!is.nan(region_hex_pop[,1]),]
+# populated_hexes <- region_hex[!is.nan(region_hex_pop[,1]),]
+# plot(populated_hexes)
+# region_hex_pop <- region_hex_pop[!is.nan(region_hex_pop[,1]),]
 
 #build a location data frame
-location_data <- data.frame(location = populated_hexes$h3_index,
-                            population = region_hex_pop,
-                            x = (st_centroid(populated_hexes) %>% st_coordinates())[,1],
-                            y = (st_centroid(populated_hexes) %>% st_coordinates())[,2])
+location_data <- data.frame(location = hexes$h3_index,
+                            population = region_hex_pop[, 1],
+                            x = (st_centroid(hexes) %>% st_coordinates())[,1],
+                            y = (st_centroid(hexes) %>% st_coordinates())[,2])
 
 location_data  <- as.location_dataframe(location_data)
 # simulate movements 
 #note that the theta parameter (proportion of population moving) does not matter here since we are rescaling every column of the movement matrix to sum to 1 anyway
 predicted_flux  <- predict(radiationWithSelection(), location_data, symmetric = FALSE)
 
-
 movement_matrix <- sweep(predicted_flux$movement_matrix,
                          2,
                          colSums(predicted_flux$movement_matrix),
                          FUN = "/")
-
 
 #check if sums are sensible 
 colSums(movement_matrix)
