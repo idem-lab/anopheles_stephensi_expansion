eb7521821e364bf05da6a70e74bcbf1a147a7a5d
geryan <geryan@users.noreply.github.com>
2023-09-04 11:24:02 +1000

    correct pa data, fix year first collection data

--- R/data_preparation.R
+++ R/data_preparation.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(readxl)
 library(lubridate)
-
+library(readr)
 
 # data from https://apps.who.int/malaria/maps/threats
 mtm_raw <- read_xlsx(
@@ -11,18 +11,20 @@ mtm_raw <- read_xlsx(
 
 
 pa_data <- mtm_raw %>%
-  filter(VECTOR_SPECIES_COMPLEX == "An. stephensi" | VECTOR_SPECIES == "An. stephensi") %>% # the papers cited here have no mention of stephensi, unclear why in this data set
   mutate(
     #date = paste(YEAR_END, MONTH_END) %>% ym
-    date = case_when(
-      !is.na(MONTH_START) ~ paste(YEAR_START, MONTH_START) %>% ym,
-      !is.na(YEAR_START) ~  paste(YEAR_START, "JAN") %>% ym,
-      TRUE ~ NA_Date_
-    ),
-    presence = if_else(INVASIVE_STATUS == "not found", 0, 1) # either listed as "invasive", or "native" if present, or "not found" if absent
+    # date = case_when(
+    #   !is.na(MONTH_START) ~ paste(YEAR_START, MONTH_START, "01") %>% ymd,
+    #   !is.na(YEAR_START) ~  paste(YEAR_START, "JAN", "01") %>% ymd,
+    #   TRUE ~ NA_Date_
+    # ), # something sus going on
+    year = YEAR_START,
+    presence = if_else(INVASIVE_STATUS == "not found", 0, 1), # either listed as "invasive", or "native" if present, or "not found" if absent
+    presence = if_else(is.na(presence), 1, presence)
   ) %>%
   select(
-    date,
+    #date,
+    year,
     presence,
     x = LONGITUDE,
     y = LATITUDE,
@@ -39,4 +41,46 @@ pa_data <- mtm_raw %>%
 saveRDS(
   pa_data,
   "output/tabular/stephensi_pa_data.RDS"
+)
+
+
+# convert to time series of 
+
+# col for ever detected
+# col for earlierst detection or for last year if not detected
+
+min_year <- min(pa_data$year, na.rm = TRUE) # 1984
+max_year <- max(pa_data$year, na.rm = TRUE) # 2022
+
+first_detection <- pa_data %>%
+  select(x, y, year, presence) %>%
+  mutate(
+    year = if_else(is.na(year), min_year, year) # number of presen ces in India, Pakistan, Iran have no dates, assign as first year
+  ) %>%
+  group_by(x, y)  %>%
+  summarise(
+    presence = sum(presence),
+    year = min(year),
+    .groups = "drop"
+  ) %>%
+  mutate(
+    year_first_detected = ifelse(presence == 0, 2022, year),
+    ever_detected = ifelse(presence > 0, 1, 0)
+  ) %>%
+  select(
+    x,
+    y,
+    year_first_detected,
+    ever_detected 
+  ) %>% 
+  arrange(year_first_detected, ever_detected)
+
+saveRDS(
+  first_detection,
+  "output/tabular/first_detection.RDS"
+)
+
+write_csv(
+  first_detection,
+  "output/tabular/first_detection.csv"
 )
