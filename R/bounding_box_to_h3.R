d21bf42dea86dbf01923de785f27d464a291ff90
AugustHao <tianxiao.hao@telethonkids.org.au>
2023-09-04 10:58:11 +1000

    turned sf to h3 script to a function

--- R/bounding_box_to_h3.R
+++ R/bounding_box_to_h3.R
@@ -1,16 +1,39 @@
-#get matching h3 geometries for bounding box area
-library(h3); library(sf); library(terra)
-h3_projection <- "ESRI:102022"
+#' takes a SF object, get corresponding h3 hexagons in pre-specified projection and resolution, as
+#' SF polygons
+#'
+#' @param sf_object
+#' @param h3_crs_to
+#' @param h3_res
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_h3_from_sf <- function(
+    sf_object,
+    h3_crs_to = "ESRI:102022",
+    h3_res = 1) {
+  
+  #get hexes ID filling up the desired polygon
+  region_hex <- h3::polyfill(sf_object,
+                             res = h3_res)
+  
+  #remove possible duplicates for hexes
+  region_hex <- unique(region_hex)
+  
+  #get hexes in sf polygons
+  region_hex <- h3::h3_to_geo_boundary_sf(region_hex)
+  
+  # fix hex poly projections
+  region_hex <- sf::st_transform(region_hex,crs = h3_projection)
+  
+  return(region_hex)
+  
+}
 
 source("R/bounding_box.R")
+region_hex <- get_h3_from_sf(
+  sf_object = st_as_sf(region_shape_buffer))
 
-
-region_shape_buffer_sf <- st_as_sf(region_shape_buffer)
-
-region_hex <- polyfill(region_shape_buffer_sf, res = 1)
-region_hex <- unique(region_hex)
-region_hex <- h3_to_geo_boundary_sf(region_hex)
-
-region_hex <- st_transform(region_hex,crs = h3_projection)
 plot(region_hex)
 
