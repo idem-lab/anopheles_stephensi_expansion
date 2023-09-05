67bdafedfdd81143fd87109fd9ac7c119bb9d4d6
geryan <geryan@users.noreply.github.com>
2023-09-05 11:37:19 +1000

    add alt hexes based on extent of base maps

--- R/prepare_hexes.R
+++ R/prepare_hexes.R
@@ -15,8 +15,11 @@ source("R/bounding_box.R")
 region_hex <- get_h3_from_sf(
   sf_object = st_as_sf(region_shape_buffer))
 
+
 # filter to only hexes populated areas 
-populated <- rast("output/rasters/derived/populated_areas.tif")
+#populated <- rast("output/rasters/derived/populated_areas.tif")
+populated <- rast("output/rasters/covariates/populated.tif")
+
 
 hexes <- region_hex %>%
   sf::st_transform(crs(populated)) %>%
@@ -43,3 +46,50 @@ hex_raster <- hex_raster * populated
 
 writeRaster(hex_raster, "output/rasters/derived/hex_raster_lookup.tif")
 saveRDS(hexes, "output/hexes.RDS")
+
+# compare hex raster with populated raster and region buffer
+plot(populated, col = inferno(5)[3:4])
+plot(hex_raster, col = viridis::mako(123), add = TRUE)
+lines(region_shape_buffer, lwd = 3, col = "darkgoldenrod1")
+
+
+## alternative hex mapping
+region_extent <- ext(populated) %>%
+  vect %>%
+  st_as_sf()
+
+region_hex_alt <- get_h3_from_sf(region_extent)
+
+hexes_alt <- region_hex_alt %>%
+  sf::st_transform(crs(populated)) %>%
+  mutate(
+    pop = terra::extract(populated, ., fun = "sum", na.rm = TRUE)[, 2]
+  ) %>%
+  filter(
+    pop > 0
+  ) %>%
+  mutate(
+    id = match(h3_index, unique(h3_index))
+  ) %>%
+  select(
+    -pop
+  )
+
+# mask by populated area to make the raster need for zonal stats calculations
+hex_raster_alt <- rasterize(hexes_alt,
+                        populated,
+                        field = "id",
+                        fun = min)
+
+hex_raster_alt <- hex_raster_alt * populated
+
+
+# compare hex raster with populated raster and region buffer
+plot(populated, col = inferno(5)[3:4])
+plot(hex_raster_alt, col = viridis::plasma(123), add = TRUE)
+lines(region_shape_buffer, lwd = 3, col = "darkgoldenrod1")
+
+# compare hexes and alternative hexes
+plot(populated, col = inferno(5)[3:4])
+plot(hex_raster_alt, col = viridis::plasma(123), add = TRUE)
+plot(hex_raster, col = viridis::mako(123), add = TRUE)
